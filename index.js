3885c4066d28364c48a21e39a0c20bbe6769b048
solferi <100904710+solferi@users.noreply.github.com>
2023-03-24 09:16:30 +0800

    Major refactoring and Bugfix
    - Disabled Kaffeine/uptimebot code. uncomment if you use it to keep the app alive.
    - FIX: multiple API calls occurring per minute. Now it calls once per minute.

--- index.js
+++ index.js
@@ -24,35 +24,61 @@ const queryURL = `${process.env['BASE_URL']}${tokenQueryID}`
 const tickerDisplayID = process.env['TICKER_DISPLAY_ID']
 
 // Simple keepalive webserver for Kaffeine/Uptimebot pings
-require('http').createServer(function (req, res) { res.end(`Server is up`) })
-    .listen(process.env.PORT || 80)
+//require('http').createServer(function (req, res) { res.end(`Server is up`) })
+//    .listen(process.env.PORT || 3380)
+
+// Date format
+const datetime = new Date();
+const strftime = require('strftime');
+const strftimeLocal = strftime.timezone('+0800')
 
 // JSON getter and parser
 const axios = require(`axios`)
 
-// Discord bot
+
+// Create Discord bot and connect it
 const Eris = require(`eris`);
 const client = new Eris(`Bot ${discordToken}`, { intents: ["guilds"] });
-
-
-client.on(`ready`, () => {
-    console.log(`Bot ${client.user.id} Logged in as "${client.user.username}"`)
-    // Run the functionality (initial)
-    getPrices()
-    // Run the functionality (succeeding)
-    setInterval(getPrices, Math.max(1, updateFrequency || 1) * 60 * 1000)
-});
-
-// Get the bot to connect to Discord
 client.connect();
 
+// Eris bot events
+client.on(`ready`, () => {
+    console.log(`------------------------`);
+    console.log(`CONNECTION ESTABLISHED`);
+    console.log(`Bot # ${client.user.id} connected`);
+    console.log(`Logged in as "${client.user.username}"`);
+});
 
+
+// Create main method and run it
+const mainf = function() {
+
+    // Say hello
+    console.log(`App is started at ${strftimeLocal('%Y-%m-%d %T', new Date())}`);
+
+    // Run the functionality once...
+    getPrices();
+
+    // ...and then run repeatedly, until termination
+    setInterval(getPrices, (updateFrequency * 1000 * 60));
+
+}
+mainf();
+
+
+// Functionality method
 function getPrices() {
 
+    // New line
+    console.log(`------------------------`);
+
+
+    // Query to CoinGecko server
     axios.get(`${queryURL}`).then(res => {
 
-        // If we got a valid response
+        // If we got a valid response, process the data
         if (res.data && res.data[0].current_price && res.data[0].price_change_percentage_24h && res.data[0].last_updated) {
+
             let currentPrice = res.data[0].current_price || 0
             let priceChange = res.data[0].price_change_24h || 0
             let priceChangePercentage = res.data[0].price_change_percentage_24h || 0
@@ -67,8 +93,7 @@ function getPrices() {
                 priceDirection = `\u2192`
             }
 
-            console.log(`------------------------`)
-            console.log(`Price is ${currentPrice} as of ${queryDate.replace(`T`, ` `).replace(`Z`, ` UTC`)}`)
+            console.log(`${tickerDisplayID} is ${currencySymbol}${currentPrice} as of ${strftimeLocal('%Y-%m-%d %T', new Date())}`)
 
             // Construct the nickname then change bot's nickname (for each guild)
             // i.e. "BTC -> $50,123.50"
@@ -77,9 +102,11 @@ function getPrices() {
                 options = {
                     nick: `${tickerDisplayID} ${priceDirection} ${currencySymbol}${currentPrice.toFixed(decimals)}`
                 }
+
                 client.editGuildMember(guildID, `@me`, options)
 
-                console.log(`  Updated guild ${guildID} (${guild.name})`)
+                console.log(`Updated ${guildID} (${guild.name})`)
+
             });
 
             // Construct the presence then change bot's nickname (for all guilds)
@@ -90,9 +117,11 @@ function getPrices() {
             }]
             client.editStatus(`online`, activities)
 
-        }
-        else
+        } else {
+
             console.log(`Could not load price data for ${tokenQueryID}`)
+
+        }
 
     }).catch(err => console.log('Error:', err))
 
